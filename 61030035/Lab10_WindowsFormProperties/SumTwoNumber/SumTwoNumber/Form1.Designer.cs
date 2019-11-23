7a22730ffb0b032830115093e96fe663fb6908e1
phruksa4165@outlook.com <phruksa4165@outlook.com>
2019-11-23 13:22:14 +0700

    commit1

--- 61030035/Lab10_WindowsFormProperties/SumTwoNumber/SumTwoNumber/Form1.Designer.cs
+++ 61030035/Lab10_WindowsFormProperties/SumTwoNumber/SumTwoNumber/Form1.Designer.cs
@@ -28,92 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.firstNumberLabel = new System.Windows.Forms.Label();
-            this.secondNumberLabel = new System.Windows.Forms.Label();
-            this.firstNumberTextBox = new System.Windows.Forms.TextBox();
-            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
-            this.sumLabel = new System.Windows.Forms.Label();
-            this.calculateButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // firstNumberLabel
+            // button1
             // 
-            this.firstNumberLabel.AutoSize = true;
-            this.firstNumberLabel.Location = new System.Drawing.Point(192, 87);
-            this.firstNumberLabel.Name = "firstNumberLabel";
-            this.firstNumberLabel.Size = new System.Drawing.Size(100, 20);
-            this.firstNumberLabel.TabIndex = 0;
-            this.firstNumberLabel.Text = "First Number";
-            // 
-            // secondNumberLabel
-            // 
-            this.secondNumberLabel.AutoSize = true;
-            this.secondNumberLabel.Location = new System.Drawing.Point(192, 148);
-            this.secondNumberLabel.Name = "secondNumberLabel";
-            this.secondNumberLabel.Size = new System.Drawing.Size(124, 20);
-            this.secondNumberLabel.TabIndex = 1;
-            this.secondNumberLabel.Text = "Second Number";
-            this.secondNumberLabel.Click += new System.EventHandler(this.secondNumberLabel_Click);
-            // 
-            // firstNumberTextBox
-            // 
-            this.firstNumberTextBox.Location = new System.Drawing.Point(371, 87);
-            this.firstNumberTextBox.Name = "firstNumberTextBox";
-            this.firstNumberTextBox.Size = new System.Drawing.Size(100, 26);
-            this.firstNumberTextBox.TabIndex = 2;
-            // 
-            // secondNumberTextBox
-            // 
-            this.secondNumberTextBox.Location = new System.Drawing.Point(371, 145);
-            this.secondNumberTextBox.Name = "secondNumberTextBox";
-            this.secondNumberTextBox.Size = new System.Drawing.Size(100, 26);
-            this.secondNumberTextBox.TabIndex = 3;
-            // 
-            // sumLabel
-            // 
-            this.sumLabel.AutoSize = true;
-            this.sumLabel.Location = new System.Drawing.Point(266, 213);
-            this.sumLabel.Name = "sumLabel";
-            this.sumLabel.Size = new System.Drawing.Size(50, 20);
-            this.sumLabel.TabIndex = 4;
-            this.sumLabel.Text = "Sum :";
-            // 
-            // calculateButton
-            // 
-            this.calculateButton.Location = new System.Drawing.Point(279, 257);
-            this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(162, 31);
-            this.calculateButton.TabIndex = 5;
-            this.calculateButton.Text = "Calculate Sum";
-            this.calculateButton.UseVisualStyleBackColor = true;
-            this.calculateButton.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Location = new System.Drawing.Point(38, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // SumberNumbers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.calculateButton);
-            this.Controls.Add(this.sumLabel);
-            this.Controls.Add(this.secondNumberTextBox);
-            this.Controls.Add(this.firstNumberTextBox);
-            this.Controls.Add(this.secondNumberLabel);
-            this.Controls.Add(this.firstNumberLabel);
+            this.Controls.Add(this.button1);
             this.Name = "SumberNumbers";
             this.Text = "Sum Two Numbers";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label firstNumberLabel;
-        private System.Windows.Forms.Label secondNumberLabel;
-        private System.Windows.Forms.TextBox firstNumberTextBox;
-        private System.Windows.Forms.TextBox secondNumberTextBox;
-        private System.Windows.Forms.Label sumLabel;
-        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ 61030035/Lab10_WindowsFormProperties/folderBrowser/folderBrowser/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace folderBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFolderBrowser = new System.Windows.Forms.Button();
+            this.txtboxFolder = new System.Windows.Forms.TextBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnFolderBrowser
+            // 
+            this.btnFolderBrowser.Location = new System.Drawing.Point(42, 28);
+            this.btnFolderBrowser.Name = "btnFolderBrowser";
+            this.btnFolderBrowser.Size = new System.Drawing.Size(104, 34);
+            this.btnFolderBrowser.TabIndex = 0;
+            this.btnFolderBrowser.Text = "Browse";
+            this.btnFolderBrowser.UseVisualStyleBackColor = true;
+            this.btnFolderBrowser.Click += new System.EventHandler(this.btnFolderBrowser_Click);
+            // 
+            // txtboxFolder
+            // 
+            this.txtboxFolder.Location = new System.Drawing.Point(42, 113);
+            this.txtboxFolder.Name = "txtboxFolder";
+            this.txtboxFolder.Size = new System.Drawing.Size(187, 26);
+            this.txtboxFolder.TabIndex = 1;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(397, 28);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(333, 387);
+            this.propertyGrid1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.txtboxFolder);
+            this.Controls.Add(this.btnFolderBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFolderBrowser;
+        private System.Windows.Forms.TextBox txtboxFolder;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
