73cf833c7f7c9700d4078ca67474329084a461c1
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2019-12-18 07:11:18 +0700

    Edit

---
+++ 61030040/saveFileDialog/saveFileDialog/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saveFileDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void BtnSaveToFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = "txt file (*.txt)|*.txt|All files(*.*)|*.*"
+           saveFileDialog.RestoreDirctor = true;
+            if(saveFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+                PropertyGrid.Refresh();
+            }
+        }
+    }
+}
--- 61030040/saveFileDialog/saveFileDialog/Properties/AssemblyInfo.cs
+++ 61030040/saveFileDialog/saveFileDialog/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab10_WindowsFormProperties")]
+[assembly: AssemblyTitle("saveFileDialog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab10_WindowsFormProperties")]
+[assembly: AssemblyProduct("saveFileDialog")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ddd18abd-6742-4b1a-be86-9376d82d6aa5")]
+[assembly: Guid("6b49d695-6afa-4ea2-a888-9e8c057d5202")]
 
 // Version information for an assembly consists of the following four values:
 //
